c1aec1bf41238d284be45c6f9230d968e3fe8151
Amandha W. Barok <amandha.barok@gmail.com>
2024-05-29 10:34:57 -0400

    add two sum in python

--- topKFrequent.py
+++ topKFrequent.py
@@ -1,22 +1,21 @@
-class Solution:
-    def topKFrequent(self, nums: list[int], k: int) -> int:
-        nums.sort()
-        print(nums)
+def topKFrequent(nums, k):
+    nums.sort()
+    print(nums)
 
-        seen = 0
+    seen = 0
 
-        for i in range(len(nums)):
-            print(i)
-            if nums[i] == nums[i-1]:
-                print("entered")
-                seen += 1
+    for i in range(len(nums)):
+        print(i)
+        if nums[i] == nums[i-1]:
+            print("entered")
+            seen += 1
+        else:
+            if seen == k:
+                return nums[i -1]
             else:
-                if seen == k:
-                    return nums[i -1]
-                else:
-                    seen = 0
-        print(seen, "seen")
-        return None
+                seen = 0
+    print(seen, "seen")
+    return None
     
 
-    print(topKFrequent(self = None, nums = [1,2,2,1,4,5], k = 2))
+print(topKFrequent([1,2,2,1,4,5], 2))
