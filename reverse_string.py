85f63b0c637cbfbced5e5be00208a51d805fc7f6
Amandha W. Barok <amandha.barok@gmail.com>
2024-06-06 13:22:26 -0400

    adding a new way to solve considering the memory space

--- reverse_string.py
+++ reverse_string.py
@@ -27,3 +27,22 @@ def reverse_string(string):
 
 print(reverse_string('hello'))
 print(reverse_string('amandha'))
+
+
+# considering the memory efficiency when the string is super big, a better approach could be the string builder. 
+# this because the concatenate (+=) creates a new space in the memory everytime it is adding a new element on the string. 
+#on the other hand, the string builder will insert all the letters at once, without copying the string to add new elements
+
+def reverse_string_builder(string):
+    stack = []
+    reversed_text_builder = []
+
+    for char in string:
+        stack.append(char)
+    
+    while stack:
+        reversed_text_builder.append(stack.pop())
+
+    return ''.join(reversed_text_builder)
+
+print(reverse_string_builder('anotherexampleinabetterperformanceconsideringthememory'))
