58a02b159b45eb26f2a05f7149eccdef178ff7fd
Amandha W. Barok <amandha.barok@gmail.com>
2024-10-10 12:10:41 -0400

    add big sum algorithm

---
+++ arrays/big_sum.py
@@ -0,0 +1,10 @@
+# In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+def big_sum(arr):
+    total = 0
+    for num in arr:
+        total += num
+    return total
+
+# TEST
+print(big_sum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005])) # 5000000015
