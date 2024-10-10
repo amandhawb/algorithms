eb6a65ee7d183d503f1f693ba9ee15adedc80e72
Amandha W. Barok <amandha.barok@gmail.com>
2024-10-10 12:11:15 -0400

    adding complexity analysis

--- arrays/big_sum.py
+++ arrays/big_sum.py
@@ -1,5 +1,7 @@
 # In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
 
+# time = O(n)
+# space = O(1)
 def big_sum(arr):
     total = 0
     for num in arr:
