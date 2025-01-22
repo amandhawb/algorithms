705a1b1aeb031cf85b8bb42729e488e1acdf6ab1
Amandha W. Barok <amandha.barok@gmail.com>
2025-01-22 15:22:16 -0500

    adding new block study

---
+++ PCAP_studies/practice_block_1.py
@@ -0,0 +1,91 @@
+# 1) A program which is used to transform source file into an executable binary file is called:
+# a) A linker
+# b) A compiler --> RIGHT
+# c) An interpreter
+# d) An IDE
+
+# 2) A process in which the source code is immediately executed without translating it into a machine code is called:
+# a) linking
+# b) compilation
+# c) interpretation --> RIGHT
+# d) debugging
+
+# 3) A code written in a high-level programming language is called:
+# a) binary code
+# b) the ASCII code
+# c) source code --> RIGHT
+# d) machine code
+
+# 4) A systax is a paart of a language definition which describes the rules used to build:
+# a) the symbols from a set of dots
+# b) the data from digits
+# c) the sentences from a set of words --> RIGHT
+# d) the words from a set of symbols
+
+# 5) Drag and drop the data type names to match the given literals:
+# String = "False" "\""
+# Boolen = False True
+# Float = 1.323232 -.1
+# Integer = 1_000_000 -1
+
+# 6) Arrange the arithmetic operations in the order which reflects their priorities.
+# -- 1st: Exponentation
+# -- 2nd: Division
+# -- 3rd: Addition
+
+# 7) Arrange the arithmetic operations in the order which reflects their priorities.
+# -- 1st: Exponentation
+# -- 2nd: Multiplication
+# -- 3rd: Subtraction
+
+# 8) Which of the following expressions evaluate to a non-zero result? (select two answers):
+# a) 4 / 2 + 2 ** 1 --> CORRECT
+# b) 1 // 2 + 3 * 4 --> CORRECT
+# c) 4 / 2 - 2 ** 1
+# d) 1 ** 2 - 4 // 3
+
+# 9) Which of the following expressions evaluate to zero? (select two answers):
+# a) 1 - 2 // 3 + 4 
+# b) 1 ** 2 / 2 // 3 --> CORRECT
+# c) 1 // 3 * 3 ** 0 --> CORRECT
+# d) 4 - 3 // 2 + 1
+
+# 10) Decide which variable names are legal or illegal in Python:
+# for --> illegal
+# samplevariable --> legal
+# @home --> illegal
+# _fun --> legal
+# i_am_a_variable --> legal
+# 2nd_class --> illegal
+# 365 --> illegal
+# TRUE --> legal
+# &sleep; --> illegal
+# True --> illegal
+# true5 --> legal
+# securityinspectiorsecretagent007 --> legal
+# maxValue --> legal
+
+# 11) What is the expected output of the following code?
+a = 5
+# a = a + a
+print(a)
+# ANSWER --> 5
+
+# 12) What is the expected output of the following code?
+a = 1#+5
+a = a + a
+# a = a + 1
+print(a) # line 4
+# ANSWER --> 2
+
+# 13) Build a line of code which asks the user for an integer value and assigns it to the floow variable:
+# ANSWER --> floor = int(input("Enter floor number: "))
+
+# 14) Build a line of code which prints the values assigned to the width and height variables separated by a multiplication sign (x):
+# ANSWER --> print(width, height, sep="x")
+
+# 15) Build a line of code which asks the user for a string value and assigns it to the password variable:
+# ANSWER --> password = input("Enter password: ")
+
+# 16) Build a line of code which asks the user for a float value and assigns it to the price variable:
+# ANSWER --> price = float("Enter item price: ")
