ab7631ea2d700ae5e078c906f167ca64fe54c468
Amandha W. Barok <amandha.barok@gmail.com>
2025-02-04 17:47:09 -0500

    adding two knowledge checks for Azure certification

---
+++ AZURE_AI_studies/knowledge_check_fundamental_AI_concepts.py
@@ -0,0 +1,16 @@
+# 1. You want to create a model to predict sales of ice cream based on historic data that includes daily ice cream sales totals and weather measurements. Which Azure service should you use? 
+# a) Azure Machine Learning --> CORRECT
+# b) Azure AI Bot Service
+# c) Azure AI Language
+
+
+# 2. You work for a wildlife sanctuary and are considering using AI to identify bird species from images. Which AI service should you use to prototype your idea? 
+# a) Azure AI Vision --> CORRECT
+# b) Azure AI Search
+# c) Azure OpenAI
+
+
+# 3. A predictive app provides audio output for visually impaired users. Which principle of Responsible AI is reflected here? 
+# a) Transparency
+# b) Inclusiveness --> CORRECT
+# c) Fairness
---
+++ AZURE_AI_studies/knowledge_check_fundamentals_of_ML.py
@@ -0,0 +1,22 @@
+# 1. You want to create a model to predict the cost of heating an office building based on its size in square feet and the number of employees working there. What kind of machine learning problem is this? 
+# a) Regression --> CORRECT
+# b) Classification
+# c) Clustering
+
+
+# 2. You need to evaluate a classification model. Which metric can you use?  
+# a) Mean squared error (MSE)
+# b) Precision --> CORRECT
+# c) Silhouette
+
+
+# 3. In deep learning, what is the purpose of a loss function? 
+# a) To remove data for which no known label values are provided
+# b) To evaluate the aggregate difference between predicted and actual label values --> CORRECT
+# c) To calculate the cost of training a neural network rather than a statistical model
+
+
+# 4. What does automated machine learning in Azure Machine Learning enable you to do? 
+# a) Automatically deploy new versions of a model as they're trained
+# b) Automatically provision Azure Machine Learning workspaces for new data scientists in an organization
+# c) Automatically run multiple training jobs using different algorithms and parameters to find the best model --> CORRECT
