bf5d48c2a5c373b87938953ebfb95ea435e09907
Amandha W. Barok <amandha.barok@gmail.com>
2025-02-05 14:19:57 -0500

    adding one more exercising

---
+++ AZURE_AI_studies/knowledge_check_fundamentals_of_AI_services.py
@@ -0,0 +1,14 @@
+# 1. An application requires three separate AI services. To see the cost for each separately, what type of resource(s) should be created? 
+# a) A multi-service resource that includes all the AI services
+# b) A single-service resource for each AI service --> CORRECT
+# c) It's not possible to see costs for individual AI services
+
+# 2. After logging into one of the individual Azure studios, what is one task to complete to begin using the individual studio? 
+# a) Input a key and endpoint into the studio
+# b) Customize the API request.
+# c) Associate a resource with the studio --> CORRECT
+
+# 3. What is an Azure AI services resource? 
+# a) A bundle of several AI services in one resource --> CORRECT
+# b) An AI service to recognize faces
+# c) A single-service resource for Azure AI Search
