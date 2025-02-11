cd2884406e5e839699c60019d7b0edbaaf38d6ff
Amandha W. Barok <amandha.barok@gmail.com>
2025-02-11 16:32:54 -0500

    adding new questions

---
+++ AZURE_AI_studies/knowledge_check_NLP_Text_Analysis.py
@@ -0,0 +1,14 @@
+# 1. You want to use Azure AI Language to determine the key talking points in a text document. Which feature of the service should you use? 
+# Sentiment analysis
+# Key phrase extraction --> Correct
+# Entity detection
+
+# 2. You use Azure AI Language to perform sentiment analysis on a sentence. The confidence scores .04 positive, .36 neutral, and .60 negative are returned. What do these confidence scores indicate about the sentence sentiment? 
+# The document is positive.
+# The document is neutral.
+# The document is negative. --> Correct
+
+# 3. When might you see NaN returned for a score in language detection? 
+# When the score calculated by the service is outside the range of 0 to 1
+# When the predominant language in the text is mixed with other languages
+# When the language is ambiguous --> Correct
---
+++ AZURE_AI_studies/knowledge_check_OCR.py
@@ -0,0 +1,9 @@
+# 1. You want to extract text from images and then use Azure AI Language to analyze the text. You want developers to require only one key and endpoint to access all of your services. What kind of resource should you create in your Azure subscription? 
+# Azure AI Vision
+# Azure AI services --> Correct
+# Azure AI Language
+
+# 2. You plan to use Azure AI Vision's Read API. What results can the Read API provide? 
+# Results arranged in pages, lines, and words --> Correct
+# Only the bounding box coordinates
+# Results arranged by pages that have photographs first, then pages that exclusively have text
