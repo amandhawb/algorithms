5c431099ee3363e95270065f6f7c413ed6ec5742
Amandha W. Barok <amandha.barok@gmail.com>
2025-02-11 15:35:48 -0500

    adding questions

---
+++ AZURE_AI_studies/knowledge_check_fundamentals_of_computer_vision.py
@@ -0,0 +1,14 @@
+# 1. Computer vision is based on the manipulation and analysis of what kinds of values in an image? 
+# Timestamps in photograph metadata
+# Pixels --> Correct
+# Image file names
+
+# 2. You want to use the Azure AI Vision service to analyze images. You also want to use the Azure AI Language service to analyze text. You want developers to require only one key and endpoint to access all of your services. What kind of resource should you create in your Azure subscription? 
+# Azure AI Vision
+# Azure AI services --> Correct
+# Azure OpenAI service
+
+# 3. You want to use the Azure AI Vision service to identify the location of individual items in an image. Which of the following features should you retrieve? 
+# Objects --> Correct
+# Visual Tags
+# Dense Captions
