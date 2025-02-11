963e660fc96b80965ae9cd801da8d2efb1bae0fe
Amandha W. Barok <amandha.barok@gmail.com>
2025-02-11 15:54:48 -0500

    adding more questions

---
+++ AZURE_AI_studies/knowledge_check_facial_recognition.py
@@ -0,0 +1,14 @@
+# 1. How does the Face service indicate the location of faces in images? 
+# A pair of coordinates for each face, indicating the center of the face
+# Two pairs of coordinates for each face, indicating the location of the eyes
+# A set of coordinates for each face, defining a rectangular bounding box around the face --> Correct
+
+# 2. What is one aspect that might impair facial detection? 
+# Glasses
+# Extreme angles --> Correct
+# Fast shutter speed
+
+# 3. What two actions are required to try out the capabilities of the Face service? 
+# Create an Azure AI Search resource, and open Vision Studio
+# Create a Face resource, and open Vision Studio --> Correct
+# Create a Face resource, and open Azure AI Studio
