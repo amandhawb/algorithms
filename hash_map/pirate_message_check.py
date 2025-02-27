55367d9abb7d1095d6e02a7053b78d51e6ca5c2a
Amandha W. Barok <amandha.barok@gmail.com>
2025-02-27 09:31:26 -0800

    refactoring algorithm question for an easier understanding

--- hash_map/pirate_message_check.py
+++ hash_map/pirate_message_check.py
@@ -8,20 +8,26 @@
 
 import string
 def can_trust_message(message):
-    if len(message) < 26:
-        return False
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-    alphabet_hash_map = {letter: 0 for letter in string.ascii_lowercase}
-
-    for letter in message:
-        if letter in alphabet_hash_map:
-            alphabet_hash_map[letter] += 1
-    
-    for counter in alphabet_hash_map.values():
-        if counter == 0:
+    for letter in alphabet:
+        if letter not in message:
             return False
-    
     return True
+    # if len(message) < 26:
+    #     return False
+
+    # alphabet_hash_map = {letter: 0 for letter in string.ascii_lowercase}
+
+    # for letter in message:
+    #     if letter in alphabet_hash_map:
+    #         alphabet_hash_map[letter] += 1
+    
+    # for counter in alphabet_hash_map.values():
+    #     if counter == 0:
+    #         return False
+    
+    # return True
     
 # Contains all letters (should return True)
 print(can_trust_message("the quick brown fox jumps over the lazy dog"))  # True
